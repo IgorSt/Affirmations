62c656e8888810bc4e2b1882acd2adc8c1d08094
Igor Santos <igor.souza@poscontrole.com.br>
2021-07-17 23:00:17 -0300

    Improve Ui :art:

--- app/src/main/java/br/igorsantos/affirmations/model/Affirmation.kt
+++ app/src/main/java/br/igorsantos/affirmations/model/Affirmation.kt
@@ -1,6 +1,12 @@
 package br.igorsantos.affirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 /**
  * Created by IgorSantos on 7/9/2021.
  */
-data class Affirmation(val stringResourceId: Int)
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+    )
